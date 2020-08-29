1a6c338ce9913791015cfaeb71842850c380292b
Rishit Pandey <rishpandey8097@gmail.com>
2020-08-29 13:45:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const Discord = require('discord.js'); 
-const client = new Discord.Client();
+const Discord = require('discord.js') 
+const client = new Discord.Client()
   
 client.on('ready', () => {   
-  console.log(`Logged in as ${client.user.tag}!`); 
-});  
+  console.log(`Logged in as ${client.user.tag}!`) 
+})  
 
-const axios = require('axios');
-const countries = require("./countries.json");
-const url = 'https://api.covid19api.com/total/country/';
-const WAKE_COMMAND = '!cases';
+const axios = require('axios')
+const countries = require("./countries.json")
+const url = 'https://api.covid19api.com/total/country/'
+const WAKE_COMMAND = '!cases'
 client.on('message', async (msg) => {
-  const content = msg.content.split(/[ ,]+/);
+  const content = msg.content.split(/[ ,]+/)
   if(content[0] === WAKE_COMMAND){
     if(content.length > 2){
       msg.reply("Too many arguments...")
@@ -21,12 +21,12 @@ client.on('message', async (msg) => {
     else if(!countries[content[1]]){
       msg.reply("Wrong country format")
     }
-    else{
+    else {
       const slug = content[1]
       const payload = await axios.get(`${url}${slug}`)
-      const covidData = payload.data.pop();
+      const covidData = payload.data.pop()
       msg.reply(`Confirmed: ${covidData.Confirmed}, Deaths: ${covidData.Deaths}, Recovered: ${covidData.Recovered}, Active: ${covidData.Active} `)
     }
   }
-});
+})
 client.login('token')
