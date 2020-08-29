c5f48bb838b264e13e167cd0be8f2416db4bf815
Rishit Pandey <rishpandey8097@gmail.com>
2020-08-29 13:46:27 +0530

    Update convertcountries.js

--- convertcountries.js
+++ convertcountries.js
@@ -1,10 +1,10 @@
 
-const countries = require("./countries");
-const countriesObject = {};
+const countries = require("./countries")
+const countriesObject = {}
 countries.forEach(country => {
   countriesObject[country.Slug] = true
 });
-const FileSystem = require("fs");
+const FileSystem = require("fs")
 FileSystem.writeFile('countries.json', JSON.stringify(countriesObject), (err) => {
-  if (err) throw err;
-});
+  if (err) throw err
+})
